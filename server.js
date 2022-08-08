48547661b8248f7a7daa14d2ead7930a43b72538
burd5 <austinburdette5@gmail.com>
2022-08-08 11:08:25 -0600

    updating changes to template

--- server.js
+++ server.js
@@ -4,3 +4,14 @@ const cors = require('cors')
 const MongoClient = require('mongodb').MongoClient
 require('dotenv').config()
 
+let db,
+    dbConnectionString = process.env.DB_STRING,
+    dbName = 'book-club',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to Database')
+        db = client.db(dbName)
+        collection = db.collection('myBooks')
+    })
